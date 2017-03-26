d414b44706323958f43b55fd62d5434d970417fa
Tero Rahko <tero.rahko@vismaconsulting.fi>
2017-03-26 17:27:55 +0300

    Add module 3

---
+++ 3-creating-descriptive-statistics/3-Creating Descriptive Statistics.R
@@ -0,0 +1,39 @@
+# Set the working directory
+setwd("3-creating-descriptive-statistics")
+
+# Read a CSV data file
+cars <- read.csv("Cars.csv")
+head(cars)
+
+# Create a frequency table
+table(cars$Transmission)
+
+# Get the minimum value
+min(cars$Fuel.Economy)
+
+# Get the maximum value
+max(cars$Fuel.Economy)
+
+# Get the average value
+mean(cars$Fuel.Economy)
+
+# Get the median value
+median(cars$Fuel.Economy)
+
+# Get the quartiles
+quantile(cars$Fuel.Economy)
+
+# Get the standard deviation
+sd(cars$Fuel.Economy)
+
+# Get the total value
+sum(cars$Fuel.Economy)
+
+# Get the correlation coefficient
+cor(
+  x = cars$Cylinders,
+  y = cars$Fuel.Economy
+)
+
+# Summarize an entire table
+summary(cars)
