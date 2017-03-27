6599ff5a6b9d006b4e7382ad8ce6b1836e9a15a8
Tero Rahko <tero.rahko@vismaconsulting.fi>
2017-03-27 20:56:44 +0300

    Add Big Data module

---
+++ 6-handling-big-data/6-Handling Big Data.R
@@ -0,0 +1,64 @@
+setwd("6-handling-big-data")
+
+install.packages("ff")
+
+library(ff)
+
+# Read a CSV file as ff data frame
+irisff <- read.table.ffdf(
+  file = "Iris.csv",
+  FUN = "read.csv"
+)
+
+# Inspect the class
+class(irisff)
+
+# Inspect the column names
+names(irisff)
+
+# Inspect the first few rows
+irisff[1:5, ]
+
+install.packages("biglm")
+library(biglm)
+
+model <- biglm(
+  formula = Petal.Width ~ Petal.Length,
+  data = irisff
+)
+
+# Summarize the model
+summary(model)
+
+# Create a scatterplot
+plot(
+  x = irisff$Petal.Length[],
+  y = irisff$Petal.Width[],
+  main = "Iris Petal Length vs. Width",
+  xlab = "Petal Length (cm)",
+  ylab = "Petal Width (cm)"
+)
+
+# Next steps are manual as biglm does not provide a fitted model
+# Get the y-intercept from the model
+b <- summary(model)$mat[1,1]
+
+# Get the slope from model
+m <- summary(model)$mat[2,1]
+
+# Draw a regression line on plot
+lines(
+  x = irisff$Petal.Length[],
+  y = m * irisff$Petal.Length[] + b,
+  col = "red",
+  lwd = 3
+)
+
+# Predict new values with the model
+predict(
+  object = model,
+  newdata = data.frame(
+    Petal.Length = c(2, 5, 7),
+    Petal.Width = c(0, 0, 0) # Required by biglm
+  )
+)
